27bd41db9c1a28177756cb224152e6ed4a612dfe
Emily Daykin <emilydaykin31@gmail.com>
2023-02-07 11:42:26 +0100

    handling keyboard shortcut conflicts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+/*global chrome*/
+
 import { useState } from "react";
 import Card from "./components/Card";
 import SearchBar from "./components/SearchBar";
 
 function App() {
-  const [isSearchBarVisible, setIsSearchBarVisible] = useState(true);
+  // Not working yet...
+  chrome.commands.getAll((commands) => {
+    for (let i = 0; i < commands.length; ++i) {
+      if (commands[i].shortcut === "Ctrl+K") {
+        console.log("The shortcut 'Ctrl+K' is already used by another extension.");
+        chrome.commands.reset(() => {
+          chrome.commands.update({
+            name: "run-extension",
+            shortcut: "Ctrl+K",
+          });
+        });
+        // chrome.commands.update({
+        //   name: "run-extension",
+        //   shortcut: "Alt+Shift+K",
+        // });
+        break;
+      }
+    }
+  });
+
+  const [ifSearchEntered, setIfSearchEntered] = useState(false);
 
   const urls = {
     url1: "https://www.google.com",
@@ -13,13 +35,14 @@ function App() {
 
   return (
     <>
-      <SearchBar
-        isSearchBarVisible={isSearchBarVisible}
-        setIsSearchBarVisible={setIsSearchBarVisible}
-      />
-      <Card link={urls.url1} />
-      <Card link={urls.url2} />
-      <Card link={urls.url3} />
+      <SearchBar setIfSearchEntered={setIfSearchEntered} />
+      {ifSearchEntered && (
+        <>
+          <Card link={urls.url1} />
+          <Card link={urls.url2} />
+          <Card link={urls.url3} />
+        </>
+      )}
     </>
   );
 }
